a18f1a0e56ca1cbecef95f519e91e20186194988
Wajeed Mabroukeh <57049753+Wajeed-Mabroukeh@users.noreply.github.com>
2023-05-17 16:27:01 +0300

    Add files via upload

---
+++ src/test/java/com/advanced/project/JobBoardPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.advanced.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobBoardPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
