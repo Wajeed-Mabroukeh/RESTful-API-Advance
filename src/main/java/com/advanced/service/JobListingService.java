0b906ec7ec790fba1bcc8251f7d89e829ec1de68
wajed <wajeed.mabroukeh@gmail.com>
2023-05-22 14:33:56 +0300

    upload the file SwaggerConfig  comntaining the config

--- src/main/java/com/advanced/service/JobListingService.java
+++ src/main/java/com/advanced/service/JobListingService.java
@@ -1,5 +1,8 @@
 package com.advanced.service;
 
+import java.io.BufferedReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -57,4 +60,7 @@ public class JobListingService {
         }
     }
 
+
+
+   
 }
