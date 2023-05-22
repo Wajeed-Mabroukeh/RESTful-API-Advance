e0b5cd4f6618dac0adc5c1b6895652a99741ed90
wajed <wajeed.mabroukeh@gmail.com>
2023-05-22 14:54:43 +0300

    upload  file ExchangeRateResponse ,progase process geter rate from api

---
+++ src/main/java/com/advanced/ExchangeRateResponse.java
@@ -0,0 +1,40 @@
+package com.advanced;
+
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ExchangeRateResponse {
+    private String base;
+    private Map<String, Double> rates;
+
+    public String getBase() {
+        return base;
+    }
+
+    public void setBase(String base) {
+        this.base = base;
+    }
+
+    public Map<String, Double> getRates() {
+        return rates;
+    }
+
+    public void setRates(Map<String, Double> rates) {
+        this.rates = rates;
+    }
+
+    @Override
+    public String toString() {
+        return "ExchangeRateResponse{" +
+                "base='" + base + '\'' +
+                ", rates=" + rates +
+                '}';
+    }
+
+    public Double getRates(String currency) {
+        return rates.get(currency);
+    }
+
+}
